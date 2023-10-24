f9289635d9bf5cf5220c53d660cabef5d9f51488
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2023-10-24 08:47:20 +0200

    first commit

---
+++ lib/components/collison_block.dart
@@ -0,0 +1,15 @@
+import 'package:flame/components.dart';
+
+class CollisionBlock extends PositionComponent {
+  bool isPlatform;
+  CollisionBlock({
+    required Vector2 position,
+    required Vector2 size,
+    this.isPlatform = false,
+  }) : super(
+          position: position,
+          size: size,
+        ) {
+    // debugMode = true;
+  }
+}
---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pixel_adventure/components/level.dart';
+
+import 'components/player.dart';
+
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks, HasCollisionDetection {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+
+  late CameraComponent cam;
+  Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joystick;
+  bool showJoystick = true;
+  List<String> levelNames = [
+    'level_01',
+    'level_02',
+  ];
+  int currentLevelIndex = 0;
+
+  @override
+  FutureOr<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
+
+    _loadLevel();
+
+    if (showJoystick) {
+      addJoystick();
+    }
+
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (showJoystick) {
+      updateJoystick();
+    }
+
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      knobRadius: 64,
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.horizontalMovement = -1;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.horizontalMovement = 1;
+        break;
+      default:
+        player.horizontalMovement = 0;
+        break;
+    }
+  }
+
+  void loadNextLevel() {
+    if (currentLevelIndex < levelNames.length - 1) {
+      currentLevelIndex++;
+      _loadLevel();
+    } else {
+      // no more levels
+    }
+  }
+
+  void _loadLevel() {
+    Future.delayed(const Duration(seconds: 1), () {
+      Level world =
+          Level(levelName: levelNames[currentLevelIndex], player: player);
+
+      cam = CameraComponent.withFixedResolution(
+        world: world,
+        width: 640,
+        height: 360,
+      );
+      cam.viewfinder.anchor = Anchor.topLeft;
+
+      addAll([cam, world]);
+    });
+  }
+}
